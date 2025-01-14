d6a3463e14872ffe59198890c7f89a9718f053cc
Aman Yadav <107786152+Aman-Yadav-1@users.noreply.github.com>
2025-01-14 17:31:47 +0530

    Delete src/components/Footer.js

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-    return (
-      <div className="footer">
-        <p className='copyright'>© 2024 VibeHarbor. All rights reserved.</p>
-        <p className='madewith'>Made with ❤️ by Aman Yadav</p>
-      </div>
-    );
-  };
-  
